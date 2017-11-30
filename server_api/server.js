3007e7f559427620db4d7abf7073d31b9f1b42a6
Huck <evanallen51@gmail.com>
2017-11-30 15:37:18 -0600

    port change

--- server_api/server.js
+++ server_api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 const server = express();
 
