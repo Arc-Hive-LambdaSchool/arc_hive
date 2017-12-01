2ebd71f245a981fe5944f05610bf052a0910c1d6
Huck <evanallen51@gmail.com>
2017-12-01 16:31:07 -0600

    Added Model.js
    NOW:
    -Using a model to handle and change incoming slack data
    -Saving all incoming slack data in array

--- server_api/server.js
+++ server_api/server.js
@@ -5,16 +5,17 @@ const port = process.env.PORT || 5000;
 const Airtable = require('airtable');
 require('dotenv').config();
 const thePrecious = process.env.AIR_TABLE_KEY;
-const ATKEY = 'keySPG804go0FXK3F';
+const slackModel = require('./slackModel');
 
 Airtable.configure({
   endpointUrl: 'https://api.airtable.com/v0/appMs812ZOuhtf8Un/Table%201',
-  apiKey: ATKEY
+  apiKey: 'keySPG804go0FXK3F'
 });
 let base = Airtable.base('appMs812ZOuhtf8Un');
 
 const server = express();
 let data2 = [];
+const fullData = [];
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/arc_hive', {useMongoClient: true});
@@ -35,22 +36,22 @@ server.get('/', (req, res) => {
     console.log(record);
     return res.json({"Record": record});
   });*/
-  if (data2) {
-    res.json({data2});
+  if (data2 && fullData) {
+    res.json({data2, fullData});
     return;
   }
   res.send(data);
 });
 
 server.post('/', (req, res) => {
-  console.log('Hello world - post');
   let data = 'hello world - post';
-  console.log(req.body);
   if (req.body) {
-    // data2.push(JSON.stringify(req.body.text));
-    data2.push(req.body);
+    data2.push(JSON.stringify(req.body.text));
     data = JSON.stringify(req.body.text);
-    res.send(data);
+    const slackBlob = req.body;
+    const newPost = new slackModel(slackBlob);
+    fullData.push(newPost);
+    res.json({'link': data, 'response_type': newPost.response_type});
     return;
   }
   res.send(data);
