d0fd2d75cdc42bfd0e7ade0749d6b43b1fb5d58a
Huck <evanallen51@gmail.com>
2017-12-01 13:54:11 -0600

    stringify

--- server_api/server.js
+++ server_api/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const Airtable = require('airtable');
 require('dotenv').config();
 // const AirTable = process.env.AIR_TABLE_KEY;
-const AirTable = 'keySPG804go0FXK3F';
+const ATKEY = 'keySPG804go0FXK3F';
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com/v0/appMs812ZOuhtf8Un/Table%201',
+  apiKey: ATKEY
+});
+let base = Airtable.base('appMs812ZOuhtf8Un');
 
 const server = express();
 
@@ -17,12 +24,16 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.get('/', (req, res) => {
   console.log('Hello world - get');
   const data = 'hello world - get';
-  const data2 = AirTable;
-  if (data2) {
-    res.json({data});
-    return;
-  }
-  res.json({data});
+  const data2 = ATKEY;
+  let base = new Airtable({apiKey: ATKEY}).base('appMs812ZOuhtf8Un');
+  base('Table 1').find('recDVfMW2yBtY0Cxi', (err, record) => {
+    if (err) {
+      console.log(err);
+      return res.json({"Error": err.message, "Stack": err.stack});
+
+    }
+    return res.json({"Record": record});
+  });
 });
 
 server.post('/', (req, res) => {
@@ -30,11 +41,11 @@ server.post('/', (req, res) => {
   let data = 'hello world - post';
   console.log(req.body);
   if (req.body) {
-    data = req.body;
-    res.json({data, AirTable});
+    data = req.body.link;
+    res.send(data.stringify());
     return;
   }
-  res.json({data});
+  res.send(data.stringify());
 });
 
 server.listen(port, () => {
