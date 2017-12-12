0b68ecbc5ba7d496a5fffaadf1a3d2f68a89e2b4
Huck <evanallen51@gmail.com>
2017-12-11 21:56:35 -0600

    test 0.1

--- server_api/search.js
+++ server_api/search.js
@@ -40,9 +40,6 @@ const sendConfirmation = (slackSearch) => {
 const arcConfirmation = (slackSearch) => {
   // console.log(slackSearch);
   let slackChannel = slackSearch.cohort;
-  slackChannel = ['CS1', 'CS2', 'CS3', 'CS4', 'CS5', 'CS6', 'CS7', 'CS8', 'CS9',
-    'CS10', 'CS11', 'CS12', 'CS13', 'CS14', 'CS15', 'CS16', 'CS17', 'CS18', 'CS19', 'CS20', 'CS21',
-   'CS22', 'CS23', 'CS24'];
    if (slackChannel === 'ALL') {
      slackChannel = ['CS1', 'CS2', 'CS3', 'CS4', 'CS5', 'CS6', 'CS7', 'CS8', 'CS9',
        'CS10', 'CS11', 'CS12', 'CS13', 'CS14', 'CS15', 'CS16', 'CS17', 'CS18', 'CS19', 'CS20', 'CS21',
