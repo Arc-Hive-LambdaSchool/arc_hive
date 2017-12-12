546b761d8e0018564fd225f7f36280f173b3b12d
Huck <evanallen51@gmail.com>
2017-12-11 21:15:43 -0600

    arc cohort text field

--- server_api/server.js
+++ server_api/server.js
@@ -299,15 +299,15 @@ server.post('/arcCommands', (req, res) => {
           },
           {
             label: 'Cohort',
-            type: 'select',
+            type: 'text',
             name: 'cohort',
             optional: true,
-            options: [
+          /*  options: [
               { label: 'CS1', value: 'CS1' },
               { label: 'CS2', value: 'CS2' },
               { label: 'CS3', value: 'CS3' },
               { label: 'CS4', value: 'CS4' },
-            ],
+            ], */
           },
           {
             label: 'Brownbag?',
