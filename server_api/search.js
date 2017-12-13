bb0067177fce54d1e92838eae056b354f257bc14
Huck <evanallen51@gmail.com>
2017-12-12 19:15:27 -0600

    tyge fix test 0.5

--- server_api/search.js
+++ server_api/search.js
@@ -52,9 +52,9 @@ const arcConfirmation = (slackSearch) => {
   for (let i = 0; i < slackChan.length; i++) {
     axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
       token: process.env.SLACK_ACCESS_TOKEN,
-      // response_type: "in_channel",
+      response_type: "in_channel",
       channel: `${slackChan[i]}`,
-      text: `test`,
+      text: `<!channel>`,
       attachments: JSON.stringify([
         {
           fields: [
