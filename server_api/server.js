d471bda2765f94314b1f418a35637ce9aa7bfc2c
Huck <evanallen51@gmail.com>
2017-12-28 14:59:14 -0600

    test upload 0.5

--- server_api/server.js
+++ server_api/server.js
@@ -651,6 +651,9 @@ server.post('/recordings', (req, res) => {
   const et = JSON.parse(fs.readFileSync(tokePath, 'utf8'));
   console.log(`651: ${JSON.stringify(et)}`);
   console.log(`653: ${JSON.stringify(req.body)}`);
+  const p = req.body.content.uuid;
+  console.log(`655: ${p}`);
+  console.log(`656: ${JSON.stringify(req.body.content.uuid)}`);
   if (req.body.type === 'RECORDING_MEETING_COMPLETED') {
     const payload = {
       "iss": process.env.ZOOM_KEY,
@@ -659,7 +662,7 @@ server.post('/recordings', (req, res) => {
     const token = jwt.sign(payload, process.env.ZOOM_SECRET);
     const g = {
       method: 'GET',
-      uri: 'https://api.zoom.us/v2/meetings/' + req.body.content.uuid + '/recordings',
+      uri: 'https://api.zoom.us/v2/meetings/' + p + '/recordings',
       headers: {
         Authorization: 'Bearer' + token,
         "alg": 'HS256',
