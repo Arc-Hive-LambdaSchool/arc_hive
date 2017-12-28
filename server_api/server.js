fbf86de0169ecdcea504c3b2a6bed0e6f00eb23a
Huck <evanallen51@gmail.com>
2017-12-28 17:37:54 -0600

    test upload 1.9

--- server_api/server.js
+++ server_api/server.js
@@ -680,12 +680,7 @@ server.post('/recordings', (req, res) => {
         const gAuth = JSON.parse(fs.readFileSync(tokePath, 'utf8'));
         const auth = new googleAuth();
         const oauth2Client = new auth.OAuth2(creds.clientId, creds.clientSecret, creds.redirectUrl);
-        oauth2Client.setCredentials({
-          access_token: gAuth.credentials.access_token,
-          refresh_token: gAuth.credentials.refresh_token,
-          token_type: gAuth.credentials.token_type,
-          expiry_date: gAuth.credentials.expiry_date,
-        });
+        oauth2Client.credentials = gAuth.credentials;
         const service = google.youtube('v3');
         const parameters = requestData['params'];
         parameters['auth'] = oauth2Client;
