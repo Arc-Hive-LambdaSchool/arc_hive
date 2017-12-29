84cfb6a31ac64b27bb188e947f299b172107d531
Huck <evanallen51@gmail.com>
2017-12-28 18:23:53 -0600

    test upload 2.4

--- server_api/server.js
+++ server_api/server.js
@@ -684,7 +684,7 @@ server.post('/recordings', (req, res) => {
         const service = google.youtube('v3');
         const parameters = requestData['params'];
         parameters['auth'] = oauth2Client;
-        parameters['media'] = { "body": requestData.mediaFilename };
+        parameters['media'] = { "body": fs.createReadStream(requestData.mediaFilename) };
         parameters['notifySubscribers'] = false;
         parameters['resource'] = requestData['properties'];
         console.log(`687: ${JSON.stringify(parameters)}`);
