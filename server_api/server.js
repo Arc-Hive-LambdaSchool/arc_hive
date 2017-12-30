d2acb8aedeeede9a36038b9ce819954c09417b68
Huck <evanallen51@gmail.com>
2017-12-29 18:03:54 -0600

    test upload 2.9

--- server_api/server.js
+++ server_api/server.js
@@ -703,12 +703,12 @@ server.post('/recordings', (req, res) => {
       };
 
       const readStream = fs.createReadStream(body.recording_files[0].download_url);
-      // readStream.on('open', () => {
-      //   readStream.pipe(res);
-      // });
-      // readStream.on('error', (err) => {
-      //   res.end(err);
-      // });
+      let data;
+      readStream.on('data', (chunk) => {
+        data += chunck;
+      }).on('end', () => {
+        console.log(data);
+      });
       const params = {
         'params': {
           'part': 'snippet,status'
