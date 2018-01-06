99b51872abc02fe7e495b82c6afecd767cf55278
Huck <evanallen51@gmail.com>
2018-01-05 19:23:36 -0600

    Jan 5 testing 0.9

--- server_api/server.js
+++ server_api/server.js
@@ -754,7 +754,6 @@ server.post('/recordings', (req, res) => {
       }).on('end', () => {
         console.log(data);
       }); */
-      const cWS = request(body.recording_files[0].download_url).pipe(fs.createWriteStream(__dirname + '/LECTUREVIDEO.mp4'));
 
       const params = {
         'params': {
@@ -773,10 +772,45 @@ server.post('/recordings', (req, res) => {
           },
           'mediaFilename': '',
         };
+        /*
+        const download = (url, dest, cb) => {
+            const file = fs.createWriteStream(dest);
+            const sendReq = request.get(url);
 
-        cWS.on('open', () => {
+            // verify response code
+            sendReq.on('response', function(response) {
+                if (response.statusCode !== 200) {
+                    return cb('Response status was ' + response.statusCode);
+                }
+            });
+
+            // check for request errors
+            sendReq.on('error', function (err) {
+                fs.unlink(dest);
+                return cb(err.message);
+            });
+
+            sendReq.pipe(file);
+
+            file.on('finish', function() {
+                file.close(cb);  // close() is async, call cb after close completes.
+            });
+
+            file.on('error', function(err) { // Handle errors
+                fs.unlink(dest); // Delete the file async. (But we don't check the result)
+                return cb(err.message);
+            });
+        };
+        */
+
+        request(body.recording_files[0].download_url).pipe(fs.createWriteStream(__dirname + '/LECTUREVIDEO.mp4').on('finish', () => {
           videosInsert(params, creds);
         });
+      );
+
+        // cWS.on('open', () => {
+        //
+        // });
       res.send('It probably worked');
       console.log('673 RESPONSE: ' + JSON.stringify(response));
       console.log('674 BODY: ' + JSON.stringify(body));
