a90496d8ba2f17f82a6bce3860327606f64b3b2a
Huck <evanallen51@gmail.com>
2018-01-09 13:13:50 -0600

    Testing Zoom Delete 0.0

--- server_api/server.js
+++ server_api/server.js
@@ -742,6 +742,13 @@ server.post('/recordings', (req, res) => {
           }
           if (data) {
             console.log(util.inspect(data, false, null));
+            request.delete('https://api.zoom.us/v2/meetings/' + p.uuid + '/recordings/' + p.recording_files[0].id, (err, response, body) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(`749 Response: ${response}`);
+              }
+            });
           }
 
         }));
