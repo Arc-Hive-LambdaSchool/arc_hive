4b9c824b700055679a377dc7eb50e10f9ae7d933
Huck <evanallen51@gmail.com>
2018-01-09 20:11:50 -0600

    Testing youtube to Airtable connection 1.2

--- server_api/server.js
+++ server_api/server.js
@@ -264,9 +264,9 @@ server.post('/', (req, res) => {
       slackSearch.airTableError(errorData);
     } else {
       const slackData = {
-        cohort: body.Cohort,
-        arcTitle: body.Title,
-        arcLink: body.Link,
+        "cohort": body.Cohort,
+        "arcTitle": body.Title,
+        "arcLink": body.Link,
       };
       console.log(`SlackData: ${JSON.stringify(slackData)}`);
       if (req.body.userId) {
