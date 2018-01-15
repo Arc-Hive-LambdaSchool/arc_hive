9cf685c101b526b1238281094473fba0f93eaa4c
Huck <evanallen51@gmail.com>
2018-01-15 13:41:17 -0600

    Testing member id Auth 0.7

--- server_api/members.js
+++ server_api/members.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const memberList = (slackUserId) => {
   const body = { token: process.env.SLACK_ACCESS_TOKEN, channel: process.env.STAFF_TEST_CHANNEL };
-  const promise = axios.get
+  const promise = axios.post
   ('https://slack.com/api/conversations.members', qs.stringify(body));
   return promise;
 };
